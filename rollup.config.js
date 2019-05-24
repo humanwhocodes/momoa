bd157023e72208fef4cbe8a4d5179e15bee7468b
Nicholas C. Zakas <nicholas@nczconsulting.com>
2019-05-24 08:45:10 -0700

    Chore: Use Rollup to create package bundle

---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    input: 'src/index.js',
+    output: {
+        file: 'api.js',
+        format: 'cjs'
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview File defining the interface of the package.
+ * @author Nicholas C. Zakas
+ */
+
+export { tokens } from "./tokens.js";
+export { parse } from "./parse.js";
