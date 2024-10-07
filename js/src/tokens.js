9b4c2564484e91542d6c2500e12ef6eaba19915f
Christopher Hiller <boneskull@boneskull.com>
2024-10-07 13:54:02 -0700

    fix(types): export ContainerNode
    This type is also useful but was not exported

--- js/src/tokens.js
+++ js/src/tokens.js
@@ -27,6 +27,7 @@ import * as charCodes from "./char-codes.js";
 /** @typedef {import("./typedefs.ts").Range} Range */
 /** @typedef {import("./typedefs.ts").Token} Token */
 /** @typedef {import("./typedefs.js").LocationRange} LocationRange */
+/** @typedef {import("./typedefs.js").ContainerNode} ContainerNode */
 /** @typedef {import("./typedefs.ts").TokenType} TokenType */
 /** @typedef {import("./typedefs.ts").TokenizeOptions} TokenizeOptions */
 
