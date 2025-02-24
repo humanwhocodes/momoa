2c77876ae452e75ec35d1120f25baabdf6fc8194
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2025-02-24 18:54:02 -0500

    fix: CommonJS types compatibility (#179)
    * fix: CommonJS types compatibility
    
    * Fix tsconfig
    
    * Try again
    
    * And again
    
    * Once more with feeling

---
+++ js/tools/update-cts-references.js
@@ -0,0 +1,23 @@
+
+/**
+ * @fileoverview Updates types.js references to types.cjs in the rolled-up
+ * .d.ts file.
+ * @author Nicholas C. Zakas
+ */
+
+//-----------------------------------------------------------------------------
+// Imports
+//-----------------------------------------------------------------------------
+
+import fs from "node:fs";
+
+//-----------------------------------------------------------------------------
+// Main
+//-----------------------------------------------------------------------------
+
+const filePath = "./dist/momoa.d.cts";
+const code = fs.readFileSync(filePath, "utf8");
+const fixedCode = code.replace(/typedefs\.js/g, "typedefs.cjs");
+
+fs.writeFileSync(filePath, fixedCode, "utf8");
+fs.copyFileSync("./dist/typedefs.d.ts", "./dist/typedefs.d.cts");
